eaf88ee68bb3eea121b499efca5c6ad6db9d15ff
Roman <rLuzinovtrainee@gmail.com>
2026-06-08 19:26:46 +0300

    Внесены контрольные правки

--- plugins/blocks-smarttech/build/block-hero-main/index.asset.php
+++ plugins/blocks-smarttech/build/block-hero-main/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '13f06b83949d847f997a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c98ef5016f7da268efe2');
--- plugins/blocks-smarttech/build/block-hero-main/index.js
+++ plugins/blocks-smarttech/build/block-hero-main/index.js
@@ -331,7 +331,7 @@ module.exports = window["wp"]["i18n"];
   \****************************************/
 (module) {
 
-module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"blocks-smarttech/block-hero-main","version":"0.1.0","title":"Block Hero for Main page","category":"smarttech","icon":"smiley","description":"Example block scaffolded with Create Block tool.","example":{},"supports":{"html":false},"attributes":{"videoUrl":{"type":"string","default":""},"imageUrl":{"type":"string","default":""},"title":{"type":"string","default":"Проектирование и строительство знаковых объектов недвижимости"},"buttonText":{"type":"string","default":"Заказать консультацию"},"buttonUrl":{"type":"string","default":"#popup:infoblock"}},"editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","viewScript":"file:./view.js"}');
+module.exports = /*#__PURE__*/JSON.parse('{"$schema":"https://schemas.wp.org/trunk/block.json","apiVersion":3,"name":"blocks-smarttech/block-hero-main","version":"0.1.0","title":"Block Hero for Main page","category":"smarttech","icon":"smiley","description":"Example block scaffolded with Create Block tool.","example":{},"supports":{"html":false},"attributes":{"videoUrl":{"type":"string","default":""},"imageUrl":{"type":"string","default":""},"title":{"type":"string","default":"Разработка концепции, архитектура, проектирование, продуктовый девелопмент отельной недвижимости"},"buttonText":{"type":"string","default":"Заказать консультацию"},"buttonUrl":{"type":"string","default":"#popup:infoblock"}},"editorScript":"file:./index.js","editorStyle":"file:./index.css","style":"file:./style-index.css","viewScript":"file:./view.js"}');
 
 /***/ }
 
